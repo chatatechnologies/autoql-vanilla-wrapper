0824720d843b327061ea1ccadcf2705ab8ee8ea9
Shiv Odedra <sodedra@chata.ai>
2025-01-28 14:05:30 -0700

    Feature: - Export react-autoql Dashboard component vanilla wrapper - Create cjs and esm bundles from build

---
+++ src/wrappers/DashboardWrapper.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Dashboard } from 'react-autoql';
+import 'react-autoql/dist/autoql.esm.css'
+
+const DashboardWrapper = ({ dashboardRef, ...props }) => {
+  const [tiles, setTiles] = React.useState([]);
+
+  React.useEffect(() => {
+    if (props.dashboardTiles) {
+      try {
+        setTiles([...props.dashboardTiles]);
+      } catch (error) {
+        console.error('Error setting tiles in DashboardWrapper:', error);
+      }
+    }
+  }, [props.tiles]);
+
+  const handleDashboardOnChange = newTiles => {
+    try {
+      setTiles([...newTiles]);
+      if (props.onChange) {
+        props.onChange([...newTiles]);
+      }
+    } catch (error) {
+      console.error('Error handling onChange in DashboardWrapper:', error);
+    }
+  }
+
+  return (
+    <Dashboard
+      ref={dashboardRef}
+      tiles={tiles}
+      onChange={handleDashboardOnChange}
+      {...props}
+    />
+  );
+};
+
+export function renderDashboard(props = {}) {
+  const container = document.getElementById(props.targetElementId);
+
+  if (!container) {
+    console.error('Target container not found!');
+    return;
+  }
+
+  const dashboardRef = React.createRef();
+  ReactDOM.render(<DashboardWrapper dashboardRef={dashboardRef} {...props} />, container);
+  return dashboardRef
+};
